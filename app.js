50cd6e2fe460d328b4fe6ecddedcedcec5c4f420
SorawitSakarin <sorawit.saka@gmail.com>
2022-11-15 16:09:23 +0700

    change port

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tpfxpo7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
-    app.listen(4000);
+    app.listen(process.env.PORT || 4000);
     console.log('Server connected success');
   })
   .catch(err =>{
