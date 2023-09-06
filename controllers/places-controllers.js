d6919741acc1fdc6df516612d7320bccfbef29ae
Sorawit Sakarin <sorawit.sak@axonstech.com>
2023-09-06 14:54:28 +0700

    delete deleteUser

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -144,52 +144,9 @@ const updatePlace = async (req, res, next) => {
   res.status(200).json({ place: place.toObject({getters:true}) });
 };
 
-const deletePlace = async (req, res, next) => {
-  const placeId = req.params.pid;
-  
-  let place;
-  try{
-    place = await Place.findById(placeId).populate('creator');
-  } catch(err) {
-    const error = new HttpError('Something went wrong, could not update place',500);
-    return next(error);
-  }
 
-  if (!place){
-    const error = new HttpError('Could not find this place',404);
-    return next(error);
-  }
-
-  if(place.creator.id !== req.userData.userId){
-    const error = new HttpError('You are not allow to delete this place',401);
-    return next(error);
-  }
-
-  const imagePath = place.image;
-
-  try{
-    const sess = await mongoose.startSession();
-    sess.startTransaction();
-    await place.remove({session:sess});
-    place.creator.places.pull(place);
-    await place.creator.save({session:sess});
-    await sess.commitTransaction();
-  } catch(err) {
-    const error = new HttpError('Something went wrong, could not update place',500);
-    return next(error);
-  }
-
-  fs.unlink(imagePath, err =>{
-    console.log(err);
-    
-  })
-  res.status(200).json({ message: "Deleted places" });
-  
-
-};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
-exports.deletePlace = deletePlace;
